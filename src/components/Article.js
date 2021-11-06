fe89e949807032f2d342350f51e7d1b61907c9b7
webster <wbian618@gmail.com>
2021-11-06 17:43:24 -0400

    completed assignment

---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Article = ({minutes, title, date ="January 1, 1970", preview}) => {
+    return (
+        <article>
+            <h3>{title}</h3>
+            <small>{date} | {minutes} min read</small>
+            <p>{preview}</p>
+
+        </article>
+    )
+}
+
+export default Article
